434866de7b62d97a7f1117e538eb377cec1ed5f8
Shvnshgrwl <shvnshgrwl1@gmail.com>
2022-08-04 07:22:18 +0530

    changes

--- Omdb/index.js
+++ Omdb/index.js
@@ -18,6 +18,18 @@ function search(){
        console.log(error)
    })
 }
+
+let url = "https://www.omdbapi.com/?apikey=642d9cdf=thor"
+
+let getData = async () =>{
+  let input = document.getElementById("search").value;
+  let url = `https://www.omdbapi.com/?s=${input}&page=1&apikey=642d9cdf`
+
+  let res = await fetch(url)
+  let data = await res.json()
+  console.log(data)
+}
+
 let id;
  let debounce = (delay,func) =>{
   if(id){
